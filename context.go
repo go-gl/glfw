76f62e20780cd6e2ed3782f43dba715fbbcc5090
Coşku Baş <cosku.bas@gmail.com>
2013-05-22 13:24:11 +0300

    Doc update

--- context.go
+++ context.go
@@ -6,22 +6,49 @@ import "C"
 
 import "unsafe"
 
+//MakeContextCurrent makes the context of the specified window current.
 func (w *Window) MakeContextCurrent() {
 	C.glfwMakeContextCurrent((*C.GLFWwindow)(unsafe.Pointer(w)))
 }
 
+//GetCurrentContext returns the window whose context is current.
 func GetCurrentContext() *Window {
 	return (*Window)(unsafe.Pointer(C.glfwGetCurrentContext()))
 }
 
+//SwapBuffers swaps the front and back buffers of the specified window. If the
+//swap interval is greater than zero, the GPU driver waits the specified number
+//of screen updates before swapping the buffers.
 func (w *Window) SwapBuffers() {
 	C.glfwSwapBuffers((*C.GLFWwindow)(unsafe.Pointer(w)))
 }
 
+//SwapInterval sets the swap interval for the current context, i.e. the number
+//of screen updates to wait before swapping the buffers of a window and
+//returning from SwapBuffers. This is sometimes called
+//'vertical synchronization', 'vertical retrace synchronization' or 'vsync'.
+//
+//Contexts that support either of the WGL_EXT_swap_control_tear and
+//GLX_EXT_swap_control_tear extensions also accept negative swap intervals,
+//which allow the driver to swap even if a frame arrives a little bit late.
+//You can check for the presence of these extensions using
+//ExtensionSupported. For more information about swap tearing,
+//see the extension specifications.
+//
+//Some GPU drivers do not honor the requested swap interval, either because of
+//user settings that override the request or due to bugs in the driver.
 func SwapInterval(interval int) {
 	C.glfwSwapInterval(C.int(interval))
 }
 
+//ExtensionSupported returns whether the specified OpenGL or context creation
+//API extension is supported by the current context. For example, on Windows
+//both the OpenGL and WGL extension strings are checked.
+//
+//As this functions searches one or more extension strings on each call, it is
+//recommended that you cache its results if it's going to be used frequently.
+//The extension strings will not change during the lifetime of a context, so
+//there is no danger in doing this.
 func ExtensionSupported(extension string) bool {
 	w := C.CString(extension)
 	defer C.free(unsafe.Pointer(w))
--- error.go
+++ error.go
@@ -5,15 +5,24 @@ package glfw
 import "C"
 
 const (
-	NotInitialized     = C.GLFW_NOT_INITIALIZED
-	NoCurrentContext   = C.GLFW_NO_CURRENT_CONTEXT
-	InvalidEnum        = C.GLFW_INVALID_ENUM
-	InvalidValue       = C.GLFW_INVALID_VALUE
-	OutOfMemory        = C.GLFW_OUT_OF_MEMORY
-	ApiUnavailable     = C.GLFW_API_UNAVAILABLE
+	//GLFW has not been initialized.
+	NotInitialized = C.GLFW_NOT_INITIALIZED
+	//No context is current.
+	NoCurrentContext = C.GLFW_NO_CURRENT_CONTEXT
+	//One of the enum parameters for the function was given an invalid enum.
+	InvalidEnum = C.GLFW_INVALID_ENUM
+	//One of the parameters for the function was given an invalid value.
+	InvalidValue = C.GLFW_INVALID_VALUE
+	//A memory allocation failed.
+	OutOfMemory = C.GLFW_OUT_OF_MEMORY
+	//GLFW could not find support for the requested client API on the system.
+	ApiUnavailable = C.GLFW_API_UNAVAILABLE
+	//The requested client API version is not available.
 	VersionUnavailable = C.GLFW_VERSION_UNAVAILABLE
-	PlatformError      = C.GLFW_PLATFORM_ERROR
-	FormatUnavailable  = C.GLFW_FORMAT_UNAVAILABLE
+	//A platform-specific error occurred that does not match any of the more specific categories.
+	PlatformError = C.GLFW_PLATFORM_ERROR
+	//The clipboard did not contain data in the requested format.
+	FormatUnavailable = C.GLFW_FORMAT_UNAVAILABLE
 )
 
 type goErrorFunc func(int, string) // Function signature to callback
@@ -24,6 +33,10 @@ func goErrorCB(err C.int, desc *C.char) {
 	fErrorHolder(int(err), C.GoString(desc))
 }
 
+//SetErrorCallback sets the error callback, which is called with an error code
+//and a human-readable description each time a GLFW error occurs.
+//
+//This function may be called before Init.
 func SetErrorCallback(cbfun goErrorFunc) {
 	fErrorHolder = cbfun
 	C.glfwSetErrorCallbackCB()
