fbf89d917ee81137148d17266f55a9e8604b520a
Cosku Bas <cosku.bas@gmail.com>
2013-06-30 09:14:16 +0300

    Move gamma to monitor

--- gamma.c
+++
@@ -1,7 +0,0 @@
-unsigned int GetGammaAtIndex(unsigned short *color, int i) {
-	return color[i];
-}
-
-void SetGammaAtIndex(unsigned short *color, int i, unsigned short value) {
-	color[i] = value;
-}
--- monitor.c
+++ monitor.c
@@ -15,3 +15,11 @@ void glfwMonitorCB(GLFWmonitor* monitor, int event) {
 void glfwSetMonitorCallbackCB() {
 	glfwSetMonitorCallback(glfwMonitorCB);
 }
+
+unsigned int GetGammaAtIndex(unsigned short *color, int i) {
+	return color[i];
+}
+
+void SetGammaAtIndex(unsigned short *color, int i, unsigned short value) {
+	color[i] = value;
+}
