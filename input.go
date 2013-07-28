ddefd3885ece247349f2cbe37920432bf075e895
user <user@ubuntu.(none)>
2013-07-29 00:50:30 +0200

    Changed a const name

--- input.go
+++ input.go
@@ -94,7 +94,7 @@ const (
 	KeyZ            Key = C.GLFW_KEY_Z
 	KeyLeftBracket  Key = C.GLFW_KEY_LEFT_BRACKET
 	KeyBackslash    Key = C.GLFW_KEY_BACKSLASH
-	KeyBracket      Key = C.GLFW_KEY_RIGHT_BRACKET
+	KeyRightBracket Key = C.GLFW_KEY_RIGHT_BRACKET
 	KeyGraveAccent  Key = C.GLFW_KEY_GRAVE_ACCENT
 	KeyWorld1       Key = C.GLFW_KEY_WORLD_1
 	KeyWorld2       Key = C.GLFW_KEY_WORLD_2
