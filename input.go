dc4e3c1d53558561f3cf8bac9a7bf4ff2b7d51c1
Coşku Baş <cosku.bas@gmail.com>
2013-08-01 11:10:50 +0300

    Update input.go
    Add correct const for KeyRightBracket

--- input.go
+++ input.go
@@ -94,7 +94,8 @@ const (
 	KeyZ            Key = C.GLFW_KEY_Z
 	KeyLeftBracket  Key = C.GLFW_KEY_LEFT_BRACKET
 	KeyBackslash    Key = C.GLFW_KEY_BACKSLASH
-	KeyBracket      Key = C.GLFW_KEY_RIGHT_BRACKET
+	KeyBracket      Key = C.GLFW_KEY_RIGHT_BRACKET //Kept for backward compatbility
+	KeyRightBracket Key = C.GLFW_KEY_RIGHT_BRACKET
 	KeyGraveAccent  Key = C.GLFW_KEY_GRAVE_ACCENT
 	KeyWorld1       Key = C.GLFW_KEY_WORLD_1
 	KeyWorld2       Key = C.GLFW_KEY_WORLD_2
