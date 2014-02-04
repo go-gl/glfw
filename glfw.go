d5fbfb9f1bfa226e0fc98c52bd77a54cd6803f33
Coşku Baş <cosku.bas@gmail.com>
2014-02-04 11:20:20 +0200

    Added a dependency in Darwin for framework CoreVideo required by GLFW

--- glfw.go
+++ glfw.go
@@ -7,7 +7,7 @@ package glfw3
 
 //#cgo windows LDFLAGS: -lglfw3dll -lopengl32 -lgdi32
 //#cgo linux LDFLAGS: -lglfw
-//#cgo darwin LDFLAGS: -lglfw3 -framework Cocoa -framework OpenGL -framework IOKit
+//#cgo darwin LDFLAGS: -lglfw3 -framework Cocoa -framework OpenGL -framework IOKit -framework CoreVideo
 //#ifdef _WIN32
 //  #define GLFW_DLL
 //#endif
