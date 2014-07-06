67acaa34eee40b38434202d57bb30836851dcb1b
Dmitri Shuralyov <shurcooL@gmail.com>
2014-07-05 18:08:16 -0700

    Improve documentation style slightly.
    Use more idiomatic style for comments, which includes a space after the
    //. See
    https://code.google.com/p/go-wiki/wiki/CodeReviewComments#Comment_Senten
    ces.
    Remove references to LockOSThread Wiki example; simply mention
    runtime.LockOSThread() in README instead.
    Other than docs, there are no code or behavior changes.

--- glfw.go
+++ glfw.go
@@ -1,7 +1,5 @@
 package glfw3
 
-// Not sure about the darwin flag
-
 // Windows users: If you download the GLFW 64-bit binaries, when you copy over the contents of the lib folder make sure to rename
 // glfw3dll.a to libglfw3dll.a, it doesn't work otherwise.
 
@@ -15,31 +13,30 @@ package glfw3
 import "C"
 
 const (
-	VersionMajor    = C.GLFW_VERSION_MAJOR    //This is incremented when the API is changed in non-compatible ways.
-	VersionMinor    = C.GLFW_VERSION_MINOR    //This is incremented when features are added to the API but it remains backward-compatible.
-	VersionRevision = C.GLFW_VERSION_REVISION //This is incremented when a bug fix release is made that does not contain any API changes.
+	VersionMajor    = C.GLFW_VERSION_MAJOR    // This is incremented when the API is changed in non-compatible ways.
+	VersionMinor    = C.GLFW_VERSION_MINOR    // This is incremented when features are added to the API but it remains backward-compatible.
+	VersionRevision = C.GLFW_VERSION_REVISION // This is incremented when a bug fix release is made that does not contain any API changes.
 )
 
-//Init initializes the GLFW library. Before most GLFW functions can be used,
-//GLFW must be initialized, and before a program terminates GLFW should be
-//terminated in order to free any resources allocated during or after
-//initialization.
+// Init initializes the GLFW library. Before most GLFW functions can be used,
+// GLFW must be initialized, and before a program terminates GLFW should be
+// terminated in order to free any resources allocated during or after
+// initialization.
 //
-//If this function fails, it calls Terminate before returning. If it succeeds,
-//you should call Terminate before the program exits.
+// If this function fails, it calls Terminate before returning. If it succeeds,
+// you should call Terminate before the program exits.
 //
-//Additional calls to this function after successful initialization but before
-//termination will succeed but will do nothing.
+// Additional calls to this function after successful initialization but before
+// termination will succeed but will do nothing.
 //
-//This function may take several seconds to complete on some systems, while on
-//other systems it may take only a fraction of a second to complete.
+// This function may take several seconds to complete on some systems, while on
+// other systems it may take only a fraction of a second to complete.
 //
-//On Mac OS X, this function will change the current directory of the
-//application to the Contents/Resources subdirectory of the application's
-//bundle, if present.
+// On Mac OS X, this function will change the current directory of the
+// application to the Contents/Resources subdirectory of the application's
+// bundle, if present.
 //
-//This function may only be called from the main thread. See
-//https://code.google.com/p/go-wiki/wiki/LockOSThread
+// This function may only be called from the main thread.
 func Init() error {
 	if glfwbool(C.glfwInit()) {
 		return nil
@@ -47,26 +44,25 @@ func Init() error {
 	return <-lastError
 }
 
-//Terminate destroys all remaining windows, frees any allocated resources and
-//sets the library to an uninitialized state. Once this is called, you must
-//again call Init successfully before you will be able to use most GLFW
-//functions.
+// Terminate destroys all remaining windows, frees any allocated resources and
+// sets the library to an uninitialized state. Once this is called, you must
+// again call Init successfully before you will be able to use most GLFW
+// functions.
 //
-//If GLFW has been successfully initialized, this function should be called
-//before the program exits. If initialization fails, there is no need to call
-//this function, as it is called by Init before it returns failure.
+// If GLFW has been successfully initialized, this function should be called
+// before the program exits. If initialization fails, there is no need to call
+// this function, as it is called by Init before it returns failure.
 //
-//This function may only be called from the main thread. See
-//https://code.google.com/p/go-wiki/wiki/LockOSThread
+// This function may only be called from the main thread.
 func Terminate() {
 	C.glfwTerminate()
 }
 
-//GetVersion retrieves the major, minor and revision numbers of the GLFW
-//library. It is intended for when you are using GLFW as a shared library and
-//want to ensure that you are using the minimum required version.
+// GetVersion retrieves the major, minor and revision numbers of the GLFW
+// library. It is intended for when you are using GLFW as a shared library and
+// want to ensure that you are using the minimum required version.
 //
-//This function may be called before Init.
+// This function may be called before Init.
 func GetVersion() (major, minor, revision int) {
 	var (
 		maj C.int
@@ -78,12 +74,12 @@ func GetVersion() (major, minor, revision int) {
 	return int(maj), int(min), int(rev)
 }
 
-//GetVersionString returns a static string generated at compile-time according
-//to which configuration macros were defined. This is intended for use when
-//submitting bug reports, to allow developers to see which code paths are
-//enabled in a binary.
+// GetVersionString returns a static string generated at compile-time according
+// to which configuration macros were defined. This is intended for use when
+// submitting bug reports, to allow developers to see which code paths are
+// enabled in a binary.
 //
-//This function may be called before Init.
+// This function may be called before Init.
 func GetVersionString() string {
 	return C.GoString(C.glfwGetVersionString())
 }
