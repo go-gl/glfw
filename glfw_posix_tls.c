dbf70fca88a710d9971da5bd44f855bbb84b5225
Stephen Gutekanst <stephen.gutekanst@gmail.com>
2014-08-23 08:55:53 -0700

    Fix windows build (don't include posix things)

--- glfw_posix_tls.c
+++ glfw_posix_tls.c
@@ -1,3 +1,3 @@
-#if defined(_GLFW_COCOA) || defined(_GLFW_WIN32) || defined(_GLFW_X11) || defined(_GLFW_WAYLAND)
+#if defined(_GLFW_COCOA) || defined(_GLFW_X11) || defined(_GLFW_WAYLAND)
 	#include "glfw/src/posix_tls.c"
 #endif
