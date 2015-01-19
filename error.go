d32a1ccb7576c75e030f786e1754b6459e6bfed6
Stephen Gutekanst <stephen.gutekanst@gmail.com>
2015-01-18 19:24:01 -0700

    Make the panic error codes private.
    Now that these error codes are translated into panics rather than
    being returned to the user as Go errors. There is no reason for
    them to be public.

--- error.go
+++ error.go
@@ -11,13 +11,18 @@ import (
 // ErrorCode corresponds to an error code.
 type ErrorCode int
 
+// Error codes that are translated to panics and the programmer should not
+// expect to handle.
+const (
+	notInitialized   ErrorCode = C.GLFW_NOT_INITIALIZED    // GLFW has not been initialized.
+	noCurrentContext ErrorCode = C.GLFW_NO_CURRENT_CONTEXT // No context is current.
+	invalidEnum      ErrorCode = C.GLFW_INVALID_ENUM       // One of the enum parameters for the function was given an invalid enum.
+	invalidValue     ErrorCode = C.GLFW_INVALID_VALUE      // One of the parameters for the function was given an invalid value.
+	outOfMemory      ErrorCode = C.GLFW_OUT_OF_MEMORY      // A memory allocation failed.
+)
+
 // Error codes.
 const (
-	NotInitialized     ErrorCode = C.GLFW_NOT_INITIALIZED     // GLFW has not been initialized.
-	NoCurrentContext   ErrorCode = C.GLFW_NO_CURRENT_CONTEXT  // No context is current.
-	InvalidEnum        ErrorCode = C.GLFW_INVALID_ENUM        // One of the enum parameters for the function was given an invalid enum.
-	InvalidValue       ErrorCode = C.GLFW_INVALID_VALUE       // One of the parameters for the function was given an invalid value.
-	OutOfMemory        ErrorCode = C.GLFW_OUT_OF_MEMORY       // A memory allocation failed.
 	APIUnavailable     ErrorCode = C.GLFW_API_UNAVAILABLE     // GLFW could not find support for the requested client API on the system.
 	VersionUnavailable ErrorCode = C.GLFW_VERSION_UNAVAILABLE // The requested client API version is not available.
 	PlatformError      ErrorCode = C.GLFW_PLATFORM_ERROR      // A platform-specific error occurred that does not match any of the more specific categories.
@@ -76,7 +81,7 @@ func fetchError() error {
 	select {
 	case err := <-lastError:
 		switch err.Code {
-		case OutOfMemory, InvalidEnum, InvalidValue, NotInitialized, NoCurrentContext:
+		case outOfMemory, invalidEnum, invalidValue, notInitialized, noCurrentContext:
 			panic(err)
 		default:
 			return err
