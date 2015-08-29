3c9e21e2d751fca8444c8761ea45399a1355cf04
James Gray <james@james4k.com>
2015-08-28 22:03:34 -0500

    darwin: workaround for native accessors

--- v3.1/glfw/native_darwin.go
+++ v3.1/glfw/native_darwin.go
@@ -1,22 +1,29 @@
 package glfw
 
-//#define GLFW_EXPOSE_NATIVE_COCOA
-//#define GLFW_EXPOSE_NATIVE_NSGL
-//#include "glfw/include/GLFW/glfw3.h"
-//#include "glfw/include/GLFW/glfw3native.h"
-import "C"
+/*
+#define GLFW_EXPOSE_NATIVE_COCOA
+#define GLFW_EXPOSE_NATIVE_NSGL
+#include "glfw/include/GLFW/glfw3.h"
+#include "glfw/include/GLFW/glfw3native.h"
 
 // See: https://github.com/go-gl/glfw3/issues/82
-/*
-func (w *Window) GetCocoaWindow() C.id {
-	ret := C.glfwGetCocoaWindow(w.data)
+inline void *workaround_glfwGetCocoaWindow(GLFWwindow *w) {
+	return (void *)glfwGetCocoaWindow(w);
+}
+inline void *workaround_glfwGetNSGLContext(GLFWwindow *w) {
+	return (void *)glfwGetNSGLContext(w);
+}
+*/
+import "C"
+
+func (w *Window) GetCocoaWindow() uintptr {
+	ret := uintptr(C.workaround_glfwGetCocoaWindow(w.data))
 	panicError()
 	return ret
 }
 
-func (w *Window) GetNSGLContext() C.id {
-	ret := C.glfwGetNSGLContext(w.data)
+func (w *Window) GetNSGLContext() uintptr {
+	ret := uintptr(C.workaround_glfwGetNSGLContext(w.data))
 	panicError()
 	return ret
 }
-*/
