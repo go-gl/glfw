93dd25be8e28c4adfb67c6cd2cedd62068ff17c6
Peter Waller <p@pwaller.net>
2020-02-21 10:36:46 +0000

    Add scripts/grab-upstream.sh
    The purpose of this script is to make it self evident from Travis' status that
    the files in the repository match upstream.
    
    It also means that if you want to update the code, you just need to change
    GLFW_C_REVISION.txt and run scripts/grab-upstream.sh.
    
    Since it is checked, it will require less review to make such changes in the future.
    
    Since it is automated, it will be easy for someone to update it if they need to
    and not make mistakes regarding which files are included or not.

---
+++ scripts/grab-upstream.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+
+# This script fetches the glfw sources from upstream
+
+# Files we don't want in the downstream go-gl/glfw repository.
+# Users can get these from upstream.
+excludes=(
+  --exclude=glfw-*/.mailmap
+  --exclude=glfw-*/CMake
+  --exclude=glfw-*/CMakeLists.txt
+  --exclude=glfw-*/README.md
+  --exclude=glfw-*/*.in
+  --exclude=glfw-*/docs
+  --exclude=glfw-*/examples
+  --exclude=glfw-*/src/CMakeLists.txt
+  --exclude=glfw-*/src/*.in
+  --exclude=glfw-*/tests
+)
+
+grab-upstream() {
+  local godir=$1
+  if [[ ! -d $godir ]]; then
+    echo "Run with top-level directory as working directory." 1>&2
+    exit 1
+  fi
+  local rev="$(cat $godir/GLFW_C_REVISION.txt)"
+  local target=$godir/glfw
+
+  rm -r $target
+  mkdir $target
+
+  curl -s -L "https://github.com/glfw/glfw/archive/${rev}.tar.gz" |
+    tar xz \
+      --strip-components=1 \
+      --directory=$target \
+      ${excludes[@]}
+}
+
+grab-upstream v3.3/glfw
+
+if test -n "$(git status --porcelain)"; then
+
+  git status
+
+  {
+    echo
+    echo "Tree is dirty after fetching GLFW_C_REVISION from upstream."
+    echo
+    echo "To fix this:"
+    echo "  1. Update GLFW_C_REVISION.txt, then:"
+    echo "  2. Run scripts/grab-upstream.sh from the top-level directory"
+    echo "  3. Submit the result as a pull request"
+    echo
+  } 1>&2
+  exit 1
+fi
