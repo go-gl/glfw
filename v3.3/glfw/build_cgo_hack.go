83604061f108840d1e2a4687b4551c4326ed3106
James <james-lawrence@users.noreply.github.com>
2020-04-20 16:25:47 -0400

    Update v3.3/glfw/build_cgo_hack.go
    Co-Authored-By: Peter Waller <p@pwaller.net>

--- v3.3/glfw/build_cgo_hack.go
+++ v3.3/glfw/build_cgo_hack.go
@@ -10,7 +10,7 @@ package glfw
 //  - every directory we want to preserve is included here as a _ import.
 //  - this file is given a build to exclude it from the regular build.
 import (
-	// prevent go tooling from stripping out the c source files.
+	// Prevent go tooling from stripping out the c source files.
 	_ "github.com/go-gl/glfw/v3.3/glfw/glfw/deps"
 	_ "github.com/go-gl/glfw/v3.3/glfw/glfw/include/GLFW"
 	_ "github.com/go-gl/glfw/v3.3/glfw/glfw/src"
