9916a6a0585bd3ba76e8720d84f0c0764d6acff3
James <james-lawrence@users.noreply.github.com>
2020-04-20 16:25:53 -0400

    Update v3.3/glfw/glfw/deps/dummy.go
    Co-Authored-By: Peter Waller <p@pwaller.net>

--- v3.3/glfw/glfw/deps/dummy.go
+++ v3.3/glfw/glfw/deps/dummy.go
@@ -4,7 +4,7 @@
 package dummy
 
 import (
-	// prevent go tooling from stripping out the c source files.
+	// Prevent go tooling from stripping out the c source files.
 	_ "github.com/go-gl/glfw/v3.3/glfw/glfw/deps/glad"
 	_ "github.com/go-gl/glfw/v3.3/glfw/glfw/deps/mingw"
 	_ "github.com/go-gl/glfw/v3.3/glfw/glfw/deps/vs2008"
