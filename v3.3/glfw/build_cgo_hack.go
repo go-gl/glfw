041637dc103a01196257a5437f3daf97d0d9112e
James <james-lawrence@users.noreply.github.com>
2020-04-20 16:25:59 -0400

    Update v3.3/glfw/build_cgo_hack.go
    Co-Authored-By: Peter Waller <p@pwaller.net>

--- v3.3/glfw/build_cgo_hack.go
+++ v3.3/glfw/build_cgo_hack.go
@@ -5,7 +5,7 @@ package glfw
 // this file exists purely to prevent the golang toolchain from stripping
 // away the c source directories and files.
 //
-// how it works:
+// How it works:
 //  - every directory which only includes c source files receives a dummy.go file.
 //  - every directory we want to preserve is included here as a _ import.
 //  - this file is given a build to exclude it from the regular build.
