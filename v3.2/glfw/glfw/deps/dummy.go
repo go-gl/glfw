4a8ce9bcb2831146140254508b199402ab09534b
Vinod Kumar Jayarajan <techievee@gmail.com>
2020-05-21 23:29:02 +1000

    add dummy files similar to fix of #258 for version of 3.3 to make the 3.2 vendoring work properly

---
+++ v3.2/glfw/glfw/deps/dummy.go
@@ -0,0 +1,12 @@
+// +build required
+
+// Package dummy prevents go tooling from stripping the c dependencies.
+package dummy
+
+import (
+	// Prevent go tooling from stripping out the c source files.
+	_ "github.com/go-gl/glfw/v3.2/glfw/glfw/deps/KHR"
+	_ "github.com/go-gl/glfw/v3.2/glfw/glfw/deps/glad"
+	_ "github.com/go-gl/glfw/v3.2/glfw/glfw/deps/mingw"
+	_ "github.com/go-gl/glfw/v3.2/glfw/glfw/deps/vulkan"
+)
