0c4de960255c8c5d7b2b1a69984a3d723cdfaea3
Peter Waller <p@pwaller.net>
2021-03-06 14:53:49 +0000

    Make build cache a function of the glfw C sources
    This is a workaround for the issue described in #269. Go unfortunately doesn't
    consider the C source files as part of its build cache calculation. Therefore,
    invalidate it by writing a hash which is computed across the full C source
    directory.
    
    This hash is only updated when `go generate` is called, but this will at least
    help the majority of users, when glfw is updated. This is encoded in
    grab-upstream.sh which is used for updating the sources.
    
    This hash is intended to be protected from divergance by CI, too.
    
    Resolves #269.

---
+++ scripts/glfw_tree_rebuild.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# This script computes the hash of the directory called "glfw", relative to $PWD
+# at time of invocation, and writes it into the upstreamTreeSHA constant in
+# glfw_tree_rebuild.go. Invoke this in a directory containing
+# glfw_tree_rebuild.go.
+
+export GIT_INDEX_FILE=$(mktemp)
+rm $GIT_INDEX_FILE
+git update-index
+git add glfw
+HASH=$(git write-tree)
+rm $GIT_INDEX_FILE
+
+sed -Ei 's/const upstreamTreeSHA = "[0-9a-f]+"/const upstreamTreeSHA = "'${HASH}'"/' glfw_tree_rebuild.go
