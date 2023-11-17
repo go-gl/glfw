b8b00452e321ed02e0a6e9a16668065090826d59
Egon Elbre <egonelbre@gmail.com>
2023-11-18 01:09:48 +0200

    v3.1,v3.2,v3.3: add a custom cursor test program
    Add a minimal test program that can be used to test basic functionality
    of custom cursors.
    
    For #383.
    
    GitHub-Pull-Request: https://github.com/go-gl/glfw/pull/385

---
+++ v3.1/glfw/testdata/customcursor/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"image"
+	"image/color"
+	"runtime"
+
+	"github.com/go-gl/glfw/v3.1/glfw"
+)
+
+func init() {
+	// This is needed to arrange that main() runs on main thread.
+	// See documentation for functions that are only allowed to be called from the main thread.
+	runtime.LockOSThread()
+}
+
+func main() {
+	err := glfw.Init()
+	if err != nil {
+		panic(err)
+	}
+	defer glfw.Terminate()
+
+	window, err := glfw.CreateWindow(640, 480, "Custom Cursor", nil, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	window.MakeContextCurrent()
+
+	// Creating a custom cursor.
+	cursor := glfw.CreateCursor(whiteTriangle, 0, 0)
+	window.SetCursor(cursor)
+
+	for !window.ShouldClose() {
+		// Do OpenGL stuff.
+		window.SwapBuffers()
+		glfw.PollEvents()
+	}
+}
+
+var whiteTriangle = func() *image.NRGBA {
+	c := color.NRGBA{R: 0xFF, G: 0xFF, B: 0xFF, A: 0xFF}
+	const size = 16
+	m := image.NewNRGBA(image.Rect(0, 0, size, size))
+	for y := 0; y < size; y++ {
+		for x := 0; x < size-y; x++ {
+			m.SetNRGBA(x, y, c)
+		}
+	}
+	return m
+}()
