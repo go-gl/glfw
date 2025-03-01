3b121bdd76ffc5d17862eb3d7177324f4c6e3079
Dmitri Shuralyov <dmitri@shuralyov.com>
2025-03-01 14:10:21 -0500

    v3.3/glfw: format glfw_tree_rebuild.go
    [git-generate]
    cd v3.3/glfw
    go fmt ./...

--- v3.3/glfw/glfw_tree_rebuild.go
+++ v3.3/glfw/glfw_tree_rebuild.go
@@ -7,5 +7,6 @@ package glfw
 // generate` on this package. This exists to invalidate the build cache (see
 // https://github.com/go-gl/glfw/issues/269), which is unaffected by C source
 // inputs.
+//
 //lint:ignore U1000 ^
-const upstreamTreeSHA = "a4a3bc8c0f4e37695a4de3f51cd7123bc186043b" 
+const upstreamTreeSHA = "a4a3bc8c0f4e37695a4de3f51cd7123bc186043b"
